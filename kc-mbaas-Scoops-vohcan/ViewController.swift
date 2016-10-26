99bc87ba2a2c2472af35be54323309dfb5701614
Nacho Villaverde Montalvo <nachovillaverde1@gmail.com>
2016-10-26 10:12:56 +0200

    Initial Commit

---
+++ kc-mbaas-Scoops-vohcan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kc-mbaas-Scoops-vohcan
+//
+//  Created by Nacho Villaverde Montalvo on 26/10/16.
+//  Copyright © 2016 vohcan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
