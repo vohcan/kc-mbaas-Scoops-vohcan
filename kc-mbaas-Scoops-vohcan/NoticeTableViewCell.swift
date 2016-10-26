6c8dbd0a23e7a26ac9c716a040bedf0fe8039bd9
Nacho Villaverde Montalvo <nachovillaverde1@gmail.com>
2016-10-26 18:00:27 +0200

    Initial commit

---
+++ kc-mbaas-Scoops-vohcan/NoticeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  NoticeTableViewCell.swift
+//  kc-mbaas-Scoops-vohcan
+//
+//  Created by Nacho Villaverde Montalvo on 26/10/16.
+//  Copyright © 2016 vohcan. All rights reserved.
+//
+
+import UIKit
+
+class NoticeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var noticeImageView: UIImageView!
+    @IBOutlet weak var tituloLabel: UILabel!
+    @IBOutlet weak var autorLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
